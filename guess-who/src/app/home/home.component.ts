d3abfb91637f9db7874860d010340cba371d055e
kevqzhang <kcrafterzh01@gmail.com>
2022-01-12 21:41:34 -0500

    removed on init implement

--- guess-who/src/app/home/home.component.ts
+++ guess-who/src/app/home/home.component.ts
@@ -1,20 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   public numImages: number = 1;
 
 
   constructor() { }
-
-  ngOnInit(): void {
-
-  }
 
   increaseNum() {
     if(this.numImages === 20) return;
